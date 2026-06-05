b123c4800a821b14386ca361e7981901d4d2954e
Layza Nicolle <layzanicolle1@gmail.com>
2026-06-05 16:22:23 -0300

    chore: atualiza configuracoes do projeto e ajusta arquivo de API

--- src/services/api.js
+++ src/services/api.js
@@ -2,15 +2,19 @@ import axios from 'axios';
 import { useAuthStore } from '../stores/authStore';
 
 const api = axios.create({
-  baseURL: 'https://trackit-backend.vercel.app', 
+  baseURL: 'https://trackit-backend.vercel.app',
 });
 
 api.interceptors.request.use((config) => {
   const token = useAuthStore.getState().token;
+  
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+  
   return config;
+}, (error) => {
+  return Promise.reject(error);
 });
 
 export default api;
